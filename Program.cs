e89b2263d234dceefbd64109a36d3abfdf3b4648
FrankB57 <f.bardelli@laposte.net>
2019-03-13 12:00:37 +0100

    Ajout projet vide

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace X_Wing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
